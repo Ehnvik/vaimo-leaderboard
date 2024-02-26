b6556a470496629a4ad36207b6cdfbef80750456
ehnvik <gustav.ehnvik@gmail.com>
2024-02-26 15:28:48 +0100

    Updated drivers array img path

--- js/drivers.js
+++ js/drivers.js
@@ -9,114 +9,114 @@ export const drivers = [
         id: "2",
         name: "Sergio Perez",
         team: "Red Bull Racing",
-        img: "../img/sergio-perez.png"
+        img: "img/sergio-perez.png"
     },
     {
         id: "3",
         name: "George Russell",
         team: "Mercedes",
-        img: "../img/george-russell.png"
+        img: "img/george-russell.png"
     },
     {
         id: "4",
         name: "Lewis Hamilton",
         team: "Mercedes",
-        img: "../img/lewis-hamilton.png"
+        img: "img/lewis-hamilton.png"
     },
     {
         id: "5",
         name: "Charles Leclerc",
         team: "Ferrari",
-        img: "../img/charles-leclerc.png"
+        img: "img/charles-leclerc.png"
     },
     {
         id: "6",
         name: "Carlos Sainz",
         team: "Ferrari",
-        img: "../img/carlos-sainz.png"
+        img: "img/carlos-sainz.png"
     },
     {
         id: "7",
         name: "Oscar Piastri",
         team: "McLaren",
-        img: "../img/oscar-piastri.png"
+        img: "img/oscar-piastri.png"
     },
     {
         id: "8",
         name: "Lando Norris",
         team: "McLaren",
-        img: "../img/lando-norris.png"
+        img: "img/lando-norris.png"
     },
     {
         id: "9",
         name: "Lance Stroll",
         team: "Aston Martin",
-        img: "../img/lance-stroll.png"
+        img: "img/lance-stroll.png"
     },
     {
         id: "10",
         name: "Fernando Alonso",
         team: "Aston Martin",
-        img: "../img/fernando-alonso.png"
+        img: "img/fernando-alonso.png"
     },
     {
         id: "11",
         name: "Esteban Ocon",
         team: "Alpine",
-        img: "../img/esteban-ocon.png"
+        img: "img/esteban-ocon.png"
     },
     {
         id: "12",
         name: "Pierre Gasly",
         team: "Alpine",
-        img: "../img/pierre-gasly.png"
+        img: "img/pierre-gasly.png"
     },
     {
         id: "13",
         name: "Alexander Albon",
         team: "Williams",
-        img: "../img/alexander-albon.png"
+        img: "img/alexander-albon.png"
     },
     {
         id: "14",
         name: "Logan Sargeant",
         team: "Williams",
-        img: "../img/logan-sargeant.png"
+        img: "img/logan-sargeant.png"
     },
     {
         id: "15",
         name: "Daniel Ricciardo",
         team: "RB",
-        img: "../img/daniel-ricciardo.png"
+        img: "img/daniel-ricciardo.png"
     },
     {
         id: "16",
         name: "Yuki Tsunoda",
         team: "RB",
-        img: "../img/yuki-tsunoda.png"
+        img: "img/yuki-tsunoda.png"
     },
     {
         id: "17",
         name: "Valtteri Bottas",
         team: "Kick Sauber",
-        img: "../img/valtteri-bottas.png"
+        img: "img/valtteri-bottas.png"
     },
     {
         id: "18",
         name: "Guanyu Zhou",
         team: "Kick Sauber",
-        img: "../img/guanyu-zhou.png"
+        img: "img/guanyu-zhou.png"
     },
     {
         id: "19",
         name: "Kevin Magnussen",
         team: "Haas F1 Team",
-        img: "../img/kevin-magnussen.png"
+        img: "img/kevin-magnussen.png"
     },
     {
         id: "20",
         name: "Nico Hulkenberg",
         team: "Haas F1 Team",
-        img: "../img/nico-hulkenberg.png"
+        img: "img/nico-hulkenberg.png"
     }
 ];
