20afa928957b3760877cf283c27b18b1599a2e41
ehnvik <gustav.ehnvik@gmail.com>
2024-02-28 09:01:23 +0100

    Removed console log

--- js/add-users.js
+++ js/add-users.js
@@ -189,11 +189,8 @@ const displayDrivers = () => {
 
 const displayUsers = () => {
     const users = getUsers()
-    if(users) {
         let html = '<div class="users-list">'
         users.forEach(user => {
-            console.log('User: ', user);
-            console.log('firstName: ', user.firstName);
          const firstName = user.firstName[0].toUpperCase() + user.firstName.slice(1)
          const lastName = user.lastName[0].toUpperCase() + user.lastName.slice(1)
          const userImageSrc = user.driver?.img ?? 'img/404.webp';
@@ -210,7 +207,6 @@ const displayUsers = () => {
         html += '</div>'
         $('#user-list-wrapper').html(html) 
         deleteUser()
-    }
 }
 
 const initializeSlider = () => {
